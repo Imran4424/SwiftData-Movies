ca65fab1e879212726c48755485a5e83fa2a3989
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-05-26 13:57:53 +0600

    initial commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Shah Md Imran Hossain on 26/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
