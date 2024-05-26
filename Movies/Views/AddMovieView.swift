7c22a673cbb85d82b5b2bdb322e55164340d78c0
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-05-26 15:29:53 +0600

    Add Movie view done

--- Movies/Views/AddMovieView.swift
+++ Movies/Views/AddMovieView.swift
@@ -6,8 +6,12 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct AddMovieView: View {
+    @Environment(\.dismiss) private var dismiss
+    @Environment(\.modelContext) private var context
+    
     @State private var title: String = ""
     @State private var year: Int?
     
@@ -20,11 +24,45 @@ struct AddMovieView: View {
             TextField("Title", text: $title)
             TextField("Year", value: $year, format: .number)
         }
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button("Close") {
+                    dismiss()
+                }
+            }
+            
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Save") {
+                    saveMovie()
+                }
+                .disabled(!isFormValid)
+            }
+        }
+    }
+}
+
+// MARK: - Methods
+extension AddMovieView {
+    private func saveMovie() {
+        guard let year else {
+            print("Year is nil")
+            return
+        }
+        
+        let movie = Movie(title: title, year: year)
+        context.insert(movie)
+        
+        do {
+            try context.save()
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
 
 #Preview {
     NavigationStack {
         AddMovieView()
+            .modelContainer(for: [Movie.self])
     }
 }
