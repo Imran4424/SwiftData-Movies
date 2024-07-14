054df6a7d8ad266bb61ad70dd0e8c2ef5c654c04
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-07-14 13:28:18 +0600

    Delete movie

--- Movies/Views/MovieListView.swift
+++ Movies/Views/MovieListView.swift
@@ -8,14 +8,46 @@
 import SwiftData
 import SwiftUI
 
+struct ComposabeListView: View {
+    let movies: [Movie]
+    @Environment(\.modelContext) private var context
+    
+    var body: some View {
+        List {
+            ForEach(movies) {movie in
+                HStack {
+                    Text(movie.title)
+                    Spacer()
+                    Text(movie.year.description)
+                }
+            }
+            .onDelete(perform: deleteMovie)
+        }
+    }
+}
+
+// MARK: - methods
+extension ComposabeListView {
+    private func deleteMovie(indexSet: IndexSet) {
+        indexSet.forEach { index in
+            let movie = movies[index]
+            context.delete(movie)
+            
+            do {
+                try context.save()
+            } catch {
+                print("error deleting the movie: \(error.localizedDescription)")
+            }
+        }
+    }
+}
+
 struct MovieListView: View {
     @Query private var movies: [Movie]
     @State private var isAddMoviePresented = false
     
     var body: some View {
-        List(movies) { movie in
-            Text(movie.title)
-        }
+        ComposabeListView(movies: movies)
         .toolbar {
             ToolbarItem(placement: .topBarTrailing) {
                 Button {
