046c8b5c0c6f3390f50ff649d83f4bea47d7101d
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-07-26 20:34:32 +0600

    actor deletion

--- Movies/Views/ActorListView.swift
+++ Movies/Views/ActorListView.swift
@@ -8,12 +8,30 @@
 import SwiftUI
 
 struct ActorListView: View {
+    @Environment(\.modelContext) private var context
+    
     let actors: [Actor]
     
     var body: some View {
         List {
             ForEach(actors) { actor in
                 Text(actor.name)
+            }
+            .onDelete(perform: deleteActor)
+        }
+    }
+}
+
+extension ActorListView {
+    private func deleteActor(indexSet: IndexSet) {
+        indexSet.forEach { index in
+            let actor = actors[index]
+            context.delete(actor)
+            
+            do {
+                try context.save()
+            } catch {
+                print("error deleting the actor: \(error.localizedDescription)")
             }
         }
     }
